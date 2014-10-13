dd2da922a3170bbee76048548d55a2315868c9e7
weizhenwei <weizhenwei-hf@loongson.cn>
2014-10-13 21:59:41 +0800

    add basic autotool files to project

---
+++ autogen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ACLOCAL=aclocal
+AUTOHEADER=autoheader
+AUTOCONF=autoconf
+AUTOMAKE=automake
+
+echo "Running aclocal..."
+$ACLOCAL
+
+echo "Running autoheader..."
+$AUTOHEADER
+
+echo "Running autoconf..."
+$AUTOCONF
+
+echo "Running automake..."
+$AUTOMAKE --force-missing --add-missing --copy
+
+echo "Regenerate configure file done!"
+
